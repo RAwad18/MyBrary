666f2711bd77a8cad3a673479410e238df65396e
awadallah.rashed01@gmail.com <awadallah.rashed01@gmail.com>
2023-01-29 12:01:01 -0500

    added async/await...fixed errorMessage

--- routes/authors.js
+++ routes/authors.js
@@ -5,8 +5,23 @@ const Author = require('../models/author.js')
 //EVERY ROUTE IS PREPENDED WITH /AUTHORS...SEE LINE 40 IN SERVER.JS
 
 // Getting All Authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index')     //renders the view in views/authors/index.ejs
+router.get('/', async (req, res) => {
+    
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch (error) {
+        res.redirect('/')
+    }
+
 })
 
 // New Author Route - displays the form for adding a new author
@@ -19,28 +34,23 @@ router.get('/new', (req, res) => {
 })
 
 // Create Author Route
-router.post('/', (req, res) => {
-    
-    // console.log(req.body);
+router.post('/', async (req, res) => {
 
-    // create new author object using Author schema
+    // create new author object using Author schema BEFORE the try-catch block
     const createdAuthor = new Author({
         name: req.body.name
     })
 
-    // save created author to the DB
-    createdAuthor.save((err, newAuthor) => {
-        if (err) {
-            // redirect to author creation form page on error --- 
-            res.render('authors/new', {
-                author: createdAuthor,  // this makes it so author.name, on this redirect due to an error, would equal what the user had input
-                errorMessage: 'Error creating author!'
-            })
-        } else{
-            // res.redirect(`authors/${newAuthor.id}`)  --- commented out for now
-            res.redirect('authors')
-        }
-    })
+    try {
+        const newAuthor = await createdAuthor.save();
+        // res.redirect(`authors/${newAuthor.id}`)  --- commented out for now
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new', {
+            author: createdAuthor,  // this makes it so author.name, on this redirect due to an error, would equal what the user had input
+            errorMessage: 'Error creating author!'
+        })
+    }
 })
 
 module.exports = router;    
